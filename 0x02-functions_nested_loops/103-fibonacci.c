ada9ae95fd754c60ae1ed2f7e02b99da85bdc488
wishazm <azmizemoumnia@gmail.com>
2023-08-15 10:19:33 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+    int i = 0;
+    long j = 1, k = 2, sum = k;
 
-	while (k + j < 4000000)
-	{
-	k += j;
-	if (k % 2 == 0)
-	sum += k;
-	j = k - j;
-	++i;
-	}
-	printf("%ld\n", sum);
-	return (0);
+    while (k + j < 4000000)
+    {
+    k += j;
+    if (k % 2 == 0)
+    sum += k;
+    j = k - j;
+    ++i;
+    }
+    printf("%ld\n", sum);
+    return (0);
 }
