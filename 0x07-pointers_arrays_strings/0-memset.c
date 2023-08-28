ed9e959765b2519570eeb1fa39d4d7696101e4e8
wishazm <azmizemoumnia@gmail.com>
2023-08-28 16:15:03 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * _memset - this function is semailar to the function memset
- *
- * @s: A pointer to the start of the memory block you want to fill.
- * @b: The value you want to set in each byte of the memory block.
- * @n: The number of bytes to be set.
- *
- * Return: it return the new value of s
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	size_t i;
-	for (i = 0; i < n; i++)
-	s[i] = b;
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
